6a7ef24f27eacf81ce78d36f9ba018f79fa9d57c
Сергей <germes63@bk.ru>
2025-08-07 22:48:26 +0300

    бновлены ссылки на фото столиков в HTML

--- lis.py
+++ lis.py
@@ -58,7 +58,7 @@ def get_reply_keyboard(user_id=None):
     [
         KeyboardButton(
             text="💻 Веб-интерфейс", 
-            web_app=WebAppInfo(url="https://f0364461f4c0.ngrok-free.app")
+            web_app=WebAppInfo(url="https://readytoearn.vercel.apppython")
         )
     ]]
 

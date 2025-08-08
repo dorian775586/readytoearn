a96a003e245c3f0655af68b28bc2de587a343fe0
Сергей <germes63@bk.ru>
2025-08-08 16:27:34 +0300

    Удаление ненужных файлов

--- белый лис/webapp/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: '.',          // корневая папка проекта — если index.html в корне
-  base: './',         // чтобы пути в сборке были относительные
-  build: {
-    outDir: 'dist'    // папка для сборки
-  }
-});
